66d7ca8631398ea7f59312e466f49f0f1d94cb31
andycarver <andrewmcarver@gmail.com>
2016-11-09 10:29:11 -0800

    pushing random word generator

---
+++ random_word_gen/apps/randomword/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse, redirect
+import random, string
+def index(request):
+    return render(request, "randomword/index.html")
+
+def generate(request):
+    if 'attempt' in request.session:
+        request.session['attempt'] += 1
+    else:
+        request.session['attempt'] = 1
+    request.session['word'] = ''.join(random.sample(string.lowercase+string.digits,14))
+    return redirect('/')
+
+def reset(request):
+	del request.session['attempt']
+	del request.session['word']
+	return redirect('/')
